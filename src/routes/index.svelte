dd4b442cdb96a2e83143ecfe900e4a364b479544
Gabriel Pereira <gabrieldpereiram@gmail.com>
2022-03-06 22:11:32 -0300

    cambio mercadito

---
+++ src/routes/index.svelte
@@ -0,0 +1,63 @@
+<div class="bg-indigo-900 relative overflow-hidden h-screen">
+	<img
+		src="https://i.pinimg.com/474x/d3/65/50/d36550ea1b17394f63c2dff243fe636d.jpg"
+		alt="shrek bigoton"
+		class="absolute h-full w-full object-cover"
+	/>
+	<div class="inset-0 bg-black opacity-25 absolute" />
+	<header class="absolute top-0 left-0 right-0 z-20">
+		<nav class="container mx-auto px-6 md:px-12 py-4">
+			<div class="md:flex justify-center items-center">
+				<div class="flex justify-between items-center">
+					<div class="md:hidden">
+						<button class="text-white focus:outline-none">
+							<svg
+								class="h-12 w-12"
+								viewBox="0 0 24 24"
+								fill="none"
+								xmlns="http://www.w3.org/2000/svg"
+							>
+								<path
+									d="M4 6H20M4 12H20M4 18H20"
+									stroke="currentColor"
+									stroke-width="2"
+									stroke-linecap="round"
+									stroke-linejoin="round"
+								/>
+							</svg>
+						</button>
+					</div>
+				</div>
+				<!-- svelte-ignore a11y-missing-attribute -->
+				<!-- svelte-ignore a11y-missing-attribute -->
+				<div class="hidden md:flex items-center">
+					<!-- svelte-ignore a11y-missing-attribute -->
+					<a
+						href="/Mercadito"
+						class="text-lg uppercase mx-3 text-white cursor-pointer hover:text-gray-300"
+					>
+						Mercadito
+					</a>
+					<a class="text-lg uppercase mx-3 text-white cursor-pointer hover:text-gray-300"> Info </a>
+					<!-- svelte-ignore a11y-missing-attribute -->
+					<a class="text-lg uppercase mx-3 text-white cursor-pointer hover:text-gray-300">
+						Contact
+					</a>
+				</div>
+			</div>
+		</nav>
+	</header>
+	<div class="container mx-auto px-6 md:px-12 relative z-10 flex items-center py-32 xl:py-40">
+		<div class="w-full flex flex-col items-center relative z-10">
+			<h1 class="font-extrabold text-7xl text-center sm:text-8xl text-white leading-tight mt-4">
+				Life-changing
+			</h1>
+			<a
+				href="/usuarios"
+				class="block bg-gray-800 hover:bg-gray-900 py-3 px-4 text-lg text-white font-bold uppercase mt-10"
+			>
+				Start Now
+			</a>
+		</div>
+	</div>
+</div>
